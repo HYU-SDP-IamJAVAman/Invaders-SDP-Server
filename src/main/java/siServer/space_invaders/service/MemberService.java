a10c2262c04c47c0eca274e5465b7305bd82275c
jimin Hwnag <154061551+specture258@users.noreply.github.com>
2024-11-29 21:41:01 +0900

    Si 15  feature/login (#2)
    Feat: Add login feature

--- src/main/java/siServer/space_invaders/service/MemberService.java
+++ src/main/java/siServer/space_invaders/service/MemberService.java
@@ -6,12 +6,13 @@ import org.springframework.transaction.annotation.Transactional;
 import siServer.space_invaders.model.Member;
 import siServer.space_invaders.repositiry.MemberRepository;
 
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
-    private final MemberRepository memberRepository;
 
+     private final MemberRepository memberRepository;
     public Member getUserById(int id) {
         return memberRepository.findById(id).orElse(null);
     }
@@ -21,7 +22,16 @@ public class MemberService {
         if(memberRepository.findById(member.getId()).isPresent()) {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
+        if(memberRepository.findByEmail(member.getEmail()).isPresent()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
 
         memberRepository.save(member);
+    }
+
+    public String getNicknameByUsername(String username){
+        return memberRepository.findByEmail(username)
+                .map(Member::getNickname)
+                .orElse("사용자가 없습니다");
     }
 }
